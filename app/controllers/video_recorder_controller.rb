d31d062a13060d84af4501858d79d0b07ba4c9c0
Yevhen Uzhva <yuzhva@debian.debian>
2016-03-14 00:39:27 +0200

    plased all videos playback on page

--- app/controllers/video_recorder_controller.rb
+++ app/controllers/video_recorder_controller.rb
@@ -1,4 +1,7 @@
 class VideoRecorderController < ApplicationController
   def index
+    @ziggeo = Ziggeo.new('1eb1e578bfc596f031a607decb12685c', '9c6ce0e56f7ee53aaafda64be7b91ea6', '6ef0ee7153dce4d1a61dcbc7c65b248b')
+    @all_videos = []
+    @ziggeo.videos.index.map { |video| @all_videos.push(video['token']) }
   end
 end
