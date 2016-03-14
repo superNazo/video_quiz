bbd1ed02e5fa7887fa54dc921bc5c6c21793cc11
Anton Romaniv <antonromaniv96@gmail.com>
2016-03-14 20:49:09 +0200

    Added route and changed layout

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,5 @@
 class Quiz < ActiveRecord::Base
   has_many :questions
   accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
+  validates :title, presence: true
 end
