285cd587ffe58b1c81ec2cdbed3d9f1681390ccb
Yevhen Uzhva <yuzhva@debian.debian>
2016-03-14 10:04:49 +0200

    rewrote video_recorder_controller with strong parameters

--- app/controllers/video_recorder_controller.rb
+++ app/controllers/video_recorder_controller.rb
@@ -1,6 +1,18 @@
 class VideoRecorderController < ApplicationController
+  before_action :ziggeo_init, :all_videos, only: :index
+
   def index
-    @ziggeo = Ziggeo.new('1eb1e578bfc596f031a607decb12685c', '9c6ce0e56f7ee53aaafda64be7b91ea6', '6ef0ee7153dce4d1a61dcbc7c65b248b')
+  end
+
+  private
+
+  def ziggeo_init
+    @ziggeo = Ziggeo.new('1eb1e578bfc596f031a607decb12685c', +
+      + '9c6ce0e56f7ee53aaafda64be7b91ea6', +
+      + '6ef0ee7153dce4d1a61dcbc7c65b248b')
+  end
+
+  def all_videos
     @all_videos = []
     @ziggeo.videos.index.map { |video| @all_videos.push(video['token']) }
   end
