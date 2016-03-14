d0e1ccf328f521529fca90f4b049f4222d546800
Yevhen Uzhva <yuzhva@debian.debian>
2016-03-14 13:52:42 +0200

    fixed controller for passing codeship test

--- app/controllers/video_recorder_controller.rb
+++ app/controllers/video_recorder_controller.rb
@@ -7,9 +7,9 @@ class VideoRecorderController < ApplicationController
   private
 
   def ziggeo_init
-    @ziggeo = Ziggeo.new('1eb1e578bfc596f031a607decb12685c', +
-      + '9c6ce0e56f7ee53aaafda64be7b91ea6', +
-      + '6ef0ee7153dce4d1a61dcbc7c65b248b')
+    @ziggeo = Ziggeo.new('1eb1e578bfc596f031a607decb12685c',
+                         '9c6ce0e56f7ee53aaafda64be7b91ea6',
+                         '6ef0ee7153dce4d1a61dcbc7c65b248b')
   end
 
   def all_videos
