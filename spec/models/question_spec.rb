b15f7c0fa5d27258d79f755ce70049124c61a78d
nazo <ivankivnazo@gmail.com>
2016-03-29 11:52:41 +0300

    Change .coffee to .js

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe Question, type: :model do
   it { should respond_to(:content) }
 
   it { should respond_to(:quiz_id) }
+
+  it { should_not be_valid }
+
+  it 'is valid with a content and quiz_id' do
+    @question.should be_valid
+  end
+
+  it 'raises an error when saved without a content' do
+    expect { subject.save! }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  describe 'when content is too long' do
+    before { @question.content = 'q' * 151 }
+    it { should_not be_valid }
+  end
 end
