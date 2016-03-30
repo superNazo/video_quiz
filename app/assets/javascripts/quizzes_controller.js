3ecc67b2f2aef75db521f7c6752addeb89dc7871
nazo <ivankivnazo@gmail.com>
2016-03-30 09:33:46 +0300

    Fix codacy comments

--- app/assets/javascripts/quizzes_controller.js
+++ app/assets/javascripts/quizzes_controller.js
@@ -1,23 +1,23 @@
 var ready;
 
 ready = function() {
-  var check_to_hide_or_show_add_link;
-  check_to_hide_or_show_add_link = function() {
-    if ($('#questions .question .nested-fields').length === 10) {
-      $('#add_question').hide();
+  var checkToHideOrShowLink;
+  checkToHideOrShowLink = function() {
+    if ($("#questions .question .nested-fields").length === 10) {
+      $("#add_question").hide();
     } else {
-      $('#add_question').show();
+      $("#add_question").show();
     }
   };
-  $('#questions').bind('cocoon:after-insert', function() {
-    check_to_hide_or_show_add_link();
+  $("#questions").bind("cocoon:after-insert", function() {
+    checkToHideOrShowLink();
   });
-  $('#questions').bind('cocoon:after-remove', function() {
-    check_to_hide_or_show_add_link();
+  $("#questions").bind("cocoon:after-remove", function() {
+    checkToHideOrShowLink();
   });
-  check_to_hide_or_show_add_link();
+  checkToHideOrShowLink();
 };
 
 $(document).ready(ready);
 
-$(document).on('page:load', ready);
+$(document).on("page:load", ready);
